35a666bb51c46f19347b125396517f542d668408
phynias <phynias@gmail.com>
2021-12-07 21:18:55 -0800

    fix var

--- files/start.sh
+++ files/start.sh
@@ -13,7 +13,7 @@ expressvpn preferences set auto_connect true
 expressvpn connect $SERVER
 for i in $(echo $WHITELIST_DNS | sed "s/ //g" | sed "s/,/ /g")
 do
-    iptables -A xvpn_dns_ip_exceptions -d 1.1.1.1/32 -p udp -m udp --dport 53 -j ACCEPT
+    iptables -A xvpn_dns_ip_exceptions -d ${i}/32 -p udp -m udp --dport 53 -j ACCEPT
     echo "allowing dns server traffic in iptables: $i"
 done
 
