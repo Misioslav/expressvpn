0120ff0b6c3eb41c31db9b4fcbb1304e01ad4aad
phynias <phynias@gmail.com>
2021-12-07 21:19:32 -0800

    fix var

--- files/start.sh
+++ files/start.sh
@@ -14,7 +14,7 @@ expressvpn connect $SERVER
 for i in $(echo $WHITELIST_DNS | sed "s/ //g" | sed "s/,/ /g")
 do
     iptables -A xvpn_dns_ip_exceptions -d ${i}/32 -p udp -m udp --dport 53 -j ACCEPT
-    echo "allowing dns server traffic in iptables: $i"
+    echo "allowing dns server traffic in iptables: ${i}"
 done
 
 touch /var/log/temp.log
