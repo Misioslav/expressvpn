863f1e47fb135329460cfc7a7efb79fe428b177e
misioslav <>
2022-01-12 14:23:56 +0100

    env fix

--- files/activate.sh
+++ files/activate.sh
@@ -6,7 +6,7 @@ expect {
 		send "N\r"
 	}
 	"Enter activation code:" {
-		send "${CODE}\r"
+		send "$env(CODE)\r"
 	}
 	"Help improve ExpressVPN: Share crash reports, speed tests, usability diagnostics, and whether VPN connection attempts succeed. These reports never contain personally identifiable information. (Y/n)" {
 		send "n\r"
